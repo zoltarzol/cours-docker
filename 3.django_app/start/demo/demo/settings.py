113f4db32ed55aad26c3fad597a3fde671c677a6
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2023-02-01 14:40:10 +0100

    test env

--- 3.django_app/start/demo/demo/settings.py
+++ 3.django_app/start/demo/demo/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+DEBUG = os.getenv('DEBUG')
+print(DEBUG)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
